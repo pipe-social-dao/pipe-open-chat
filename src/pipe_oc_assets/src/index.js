8d5e9c70834c32ba17bb3ced51cc0c4e278cb174
pipe <357681911qq@gmail.com>
2022-02-22 16:49:51 +0800

    Initial commit.

---
+++ src/pipe_oc_assets/src/index.js
@@ -0,0 +1,19 @@
+import { pipe_oc } from "../../declarations/pipe_oc";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await pipe_oc.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
