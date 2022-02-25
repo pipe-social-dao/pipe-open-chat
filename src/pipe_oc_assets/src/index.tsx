4ada2ddb39d5ba930f2582568043716444b8b81e
pipe <357681911qq@gmail.com>
2022-02-25 20:01:02 +0800

    direct-rebuild:move css under src dir

--- src/pipe_oc_assets/src/index.tsx
+++ src/pipe_oc_assets/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { APP_TITLE } from './constants'
 import startup from './startup'
-import '../css/index.css'
+import './css/index.css'
 
 
 if(process.env.NODE_ENV === 'development'){
